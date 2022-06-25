26c28abd5c8c53ab1792e7dddacd7219acfd6ef2
ezequielhenrique <ezequielhenrique806@gmail.com>
2022-06-25 15:31:48 -0300

    Create grid input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,34 @@
 const container = document.querySelector('.container');
+const numberOfSquares = document.querySelector('#number-of-squares');
+const squaresLabel = document.querySelector("#squares-label");
 
-for (i=0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('container-square');
-    square.addEventListener('mouseover', () => {
-        square.classList.add('hover');
+createGrid(container, 16, 16);
+
+numberOfSquares.addEventListener('change', () => {
+    let square = document.querySelectorAll('.container-square');
+    let qtd = numberOfSquares.value;
+
+    square.forEach((e) => {
+        e.parentNode.removeChild(e);
     });
-    container.appendChild(square)
+
+    squaresLabel.innerHTML = `${qtd} x ${qtd}`;
+    createGrid(container, qtd, qtd);
+});
+
+numberOfSquares.addEventListener('input', () => {
+    let qtd = numberOfSquares.value;
+    squaresLabel.innerHTML = `${qtd} x ${qtd}`;
+});
+
+function createGrid(parent, rows, columns) {
+    for (i=0; i < rows*columns; i++) {
+        const square = document.createElement('div');
+        square.classList.add('container-square');
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hover');
+        });
+        parent.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+        parent.appendChild(square);
+    }
 }

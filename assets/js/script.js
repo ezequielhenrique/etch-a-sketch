2acc517bce9c4c95b8478472964c5e6db4a5d6dd
ezequielhenrique <ezequielhenrique806@gmail.com>
2022-07-11 21:43:23 -0300

    updateGrid function and Color Picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
 const container = document.querySelector('.container');
 const numberOfSquares = document.querySelector('#number-of-squares');
-const squaresLabel = document.querySelector("#squares-label");
+const squaresLabel = document.querySelector('#squares-label');
+const colorPicker = document.querySelector('#input-color');
+const buttonClear = document.querySelector('#button-clear');
+
 
 createGrid(container, 16, 16);
 
-numberOfSquares.addEventListener('change', () => {
+numberOfSquares.addEventListener('change', updateGrid);
+buttonClear.addEventListener('click', updateGrid);
+
+numberOfSquares.addEventListener('input', () => {
+    let qtd = numberOfSquares.value;
+    squaresLabel.innerHTML = `${qtd} x ${qtd}`;
+});
+
+
+function createGrid(parent, rows, columns) {
+    for (i=0; i < rows*columns; i++) {
+        const square = document.createElement('div');
+        square.classList.add('container-square');
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hover');
+            square.style.backgroundColor = colorPicker.value;
+        });
+        parent.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+        parent.appendChild(square);
+    }
+}
+
+
+function updateGrid() {
     let square = document.querySelectorAll('.container-square');
     let qtd = numberOfSquares.value;
 
@@ -14,21 +40,5 @@ numberOfSquares.addEventListener('change', () => {
 
     squaresLabel.innerHTML = `${qtd} x ${qtd}`;
     createGrid(container, qtd, qtd);
-});
-
-numberOfSquares.addEventListener('input', () => {
-    let qtd = numberOfSquares.value;
-    squaresLabel.innerHTML = `${qtd} x ${qtd}`;
-});
-
-function createGrid(parent, rows, columns) {
-    for (i=0; i < rows*columns; i++) {
-        const square = document.createElement('div');
-        square.classList.add('container-square');
-        square.addEventListener('mouseover', () => {
-            square.classList.add('hover');
-        });
-        parent.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
-        parent.appendChild(square);
-    }
 }
+

111c9de2aaff651a00e7118df07d9b5f1a51ea44
ezequielhenrique <ezequielhenrique806@gmail.com>
2024-10-08 12:54:00 -0300

    add dowload button and update logic and design

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const numberOfSquares = document.querySelector('#number-of-squares');
 const squaresLabel = document.querySelector('#squares-label');
 const colorPicker = document.querySelector('#input-color');
 const buttonClear = document.querySelector('#button-clear');
-
+const saveButton = document.querySelector('#save-button')
 
 createGrid(container, 16, 16);
 
@@ -15,20 +15,40 @@ numberOfSquares.addEventListener('input', () => {
     squaresLabel.innerHTML = `${qtd} x ${qtd}`;
 });
 
+saveButton.addEventListener('click', () => {
+    html2canvas(container).then((canvas) => {
+        var link = document.createElement('a');
+        link.download = 'desenho.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+    });
+});
+
 
 function createGrid(parent, rows, columns) {
+    let isDragging = false;
+
     for (i=0; i < rows*columns; i++) {
         const square = document.createElement('div');
+
         square.classList.add('container-square');
-        square.addEventListener('mouseover', () => {
-            square.classList.add('hover');
-            square.style.backgroundColor = colorPicker.value;
+        square.addEventListener('mousedown', () => {
+            isDragging = true;
         });
+        square.addEventListener('mousemove', () => {
+            if (isDragging) {
+                square.classList.add('hover');
+                square.style.backgroundColor = colorPicker.value;
+            }
+        });
+        square.addEventListener('mouseup', () => {
+            isDragging = false;
+        });
+
         parent.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
         parent.appendChild(square);
     }
 }
-
 
 function updateGrid() {
     let square = document.querySelectorAll('.container-square');
@@ -41,4 +61,3 @@ function updateGrid() {
     squaresLabel.innerHTML = `${qtd} x ${qtd}`;
     createGrid(container, qtd, qtd);
 }
-
